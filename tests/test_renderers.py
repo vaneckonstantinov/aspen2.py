84432075c038022b640aaeeb550b6a0e94c4f39d
Changaco <changaco@changaco.oy.lc>
2016-07-24 09:57:40 +0200

    failing tests for AspenWeb/pando.py#284
    The first two are also regression tests for AspenWeb/pando.py#8.
    The third one is salvaged from the previous PR AspenWeb/pando.py#301.

--- tests/test_renderers.py
+++ tests/test_renderers.py
@@ -3,6 +3,8 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from pytest import raises
+
 from aspen.simplates import json_
 from aspen.simplates.renderers import Factory, Renderer
 
@@ -33,3 +35,53 @@ def test_a_custom_renderer(harness):
     assert d['media_type'] == 'text/html'
     assert d['offset'] == 2
     assert d['compiled'] == 'LOREM IPSUM'
+
+
+def test_renderer_padding_works_with_padded_output(harness):
+    class TestRenderer(Renderer):
+
+        def compile(self, filepath, padded):
+            assert padded[:self.offset] == b'\n' * self.offset
+            return padded
+
+        def render_content(self, context):
+            return b'\n' + self.compiled + b'\n'
+
+    class TestFactory(Factory):
+        Renderer = TestRenderer
+
+    request_processor = harness.request_processor
+    request_processor.renderer_factories['x'] = TestFactory(request_processor)
+
+    output = harness.simple("[---]\n[---] text/plain via x\nSome text")
+    assert output.body == '\nSome text\n'
+
+
+def test_renderer_padding_works_with_stripped_output(harness):
+    class TestRenderer(Renderer):
+
+        def compile(self, filepath, padded):
+            assert padded[:self.offset] == b'\n' * self.offset
+            return padded
+
+        def render_content(self, context):
+            return b'\n' + self.raw + b'\n'
+
+    class TestFactory(Factory):
+        Renderer = TestRenderer
+
+    request_processor = harness.request_processor
+    request_processor.renderer_factories['y'] = TestFactory(request_processor)
+
+    output = harness.simple("[---]\n[---] text/plain via y\nSome text")
+    assert output.body == '\nSome text\n'
+
+
+def test_renderer_padding_achieves_correct_line_numbers_in_tracebacks(harness):
+    harness.fs.www.mk(('index.html.spt', '''\
+    [---]
+    [---] text/html via stdlib_template
+    Greetings, $!
+    '''))
+    actual = str(raises(ValueError, harness._hit, 'GET', '/').value)
+    assert 'line 3' in actual
