d2f5d94410ac3beba5d48438d14784bdb3aea372
Chad Whitacre <chad@zetaweb.com>
2016-09-15 15:12:56 -0400

    Prune algorithm utils
    Ticket to upstream:
    
    https://github.com/AspenWeb/algorithm.py/issues/19

--- aspen/utils.py
+++ aspen/utils.py
@@ -16,9 +16,6 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import codecs
-import re
-
-import algorithm
 
 
 # Register a 'repr' error strategy.
@@ -37,70 +34,3 @@ def replace_with_repr(unicode_error):
     return (r, unicode_error.end)
 
 codecs.register_error('repr', replace_with_repr)
-
-
-# Filters
-# =======
-# These are decorators for algorithm functions.
-
-def by_lambda(filter_lambda):
-    """
-    """
-    def wrap(function):
-        def wrapped_function_by_lambda(*args,**kwargs):
-            if filter_lambda():
-                return function(*args,**kwargs)
-        algorithm._transfer_func_name(wrapped_function_by_lambda, function)
-        return wrapped_function_by_lambda
-    return wrap
-
-
-def by_regex(regex_tuples, default=True):
-    """Only call function if
-
-    regex_tuples is a list of (regex, filter?) where if the regex matches the
-    requested URI, then the flow is applied or not based on if filter? is True
-    or False.
-
-    For example::
-
-        from aspen.flows.filter import by_regex
-
-        @by_regex( ( ("/secret/agenda", True), ( "/secret.*", False ) ) )
-        def use_public_formatting(request):
-            ...
-
-    would call the 'use_public_formatting' flow step only on /secret/agenda
-    and any other URLs not starting with /secret.
-
-    """
-    regex_res = [ (re.compile(regex), disposition) \
-                           for regex, disposition in regex_tuples.items() ]
-    def filter_function(function):
-        def function_filter(request, *args):
-            for regex, disposition in regex_res:
-                if regex.matches(request.line.uri):
-                    if disposition:
-                        return function(*args)
-            if default:
-                return function(*args)
-        algorithm._transfer_func_name(function_filter, function)
-        return function_filter
-    return filter_function
-
-
-def by_dict(truthdict, default=True):
-    """Filter for hooks
-
-    truthdict is a mapping of URI -> filter? where if the requested URI is a
-    key in the dict, then the hook is applied based on the filter? value.
-
-    """
-    def filter_function(function):
-        def function_filter(request, *args):
-            do_hook = truthdict.get(request.line.uri, default)
-            if do_hook:
-                return function(*args)
-        algorithm._transfer_func_name(function_filter, function)
-        return function_filter
-    return filter_function
