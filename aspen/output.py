dbbcf8a3777e5c901c0b9c4dca5969c2358d4ded
Changaco <changaco@changaco.oy.lc>
2019-09-14 17:28:53 +0200

    improve the Output class

--- aspen/output.py
+++ aspen/output.py
@@ -1,8 +1,17 @@
-class Output(object):
-    body = media_type = charset = None
+from .utils import auto_repr
 
-    def __init__(self, **kw):
-        self.__dict__.update(kw)
+
+@auto_repr
+class Output(object):
+    """The result of rendering a resource.
+    """
+
+    __slots__ = ('body', 'media_type', 'charset')
+
+    def __init__(self, body=None, media_type=None, charset=None):
+        self.body = body
+        self.media_type = media_type
+        self.charset = charset
 
     @property
     def text(self):
